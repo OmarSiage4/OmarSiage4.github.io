26bd6908c18fbfb7876ae3b9563ead910275214c
OmarSiage3 <omar.siage.work@gmail.com>
2020-06-09 21:01:47 -0400

    Radio Buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
 function transferData(){
 	var customerCategory = document.getElementById("customerCategory").value;
-	var organic = document.getElementById("organic").checked;
-	var priceSort = document.getElementById("priceSort").checked;
+	var organic = document.getElementById("organicYes").checked; 
+
+	if(organic){
+		organic="true"
+	}
+	else{
+		organic="false";
+	}
+
+
+
+	var priceSort = document.getElementById("organicYes").checked
+	if(priceSort){
+		priceSort = "true";
+	}
+	else{
+		priceSort = "false";
+	}
+
 
 	console.log(customerCategory);
 	console.log(organic);
